843a704fb83ead2504710c5cdfc6e0d61258c86f
Gabriel dos Santos <gsantos.4dev@gmail.com>
2022-04-06 18:13:26 -0300

    enter

--- js/index.js
+++ js/index.js
@@ -1,31 +1,48 @@
+// DUBMIT
 function submit(){
     document.querySelector('.caixa-box-principal').classList.remove('visivel')
     document.querySelector('.caixa-box-secundaria').classList.add('visivel')
+    document.querySelector('.rating span').innerHTML = document.querySelectorAll('.selecionado').length 
 }
-
-const documento = document.querySelector('.botoes-numeros li') 
+// BOTOES
+const numeros = [document.getElementById('1'), document.getElementById('2'), document.getElementById('3'), document.getElementById('4'), document.getElementById('5')]
 function botaoUm(){
-    console.log('eita')
-    document.querySelector('.selecionado').classList.remove('selecionado')
-    document.getElementById('1').classList.add('selecionado')
+    if( numeros[0].className == 'selecionado'){
+        numeros[0].classList.remove('selecionado')
+    }
+    else {
+        numeros[0].classList.add('selecionado')
+    }
 }
 function botaoDois(){
-    console.log('eita')
-    document.querySelector('.selecionado').classList.remove('selecionado')
-    document.getElementById('2').classList.add('selecionado')
+    if(  numeros[1].className == 'selecionado'){
+         numeros[1].classList.remove('selecionado')
+    }
+    else {
+         numeros[1].classList.add('selecionado')
+    }
 }
 function botaoTres(){
-    console.log('eita')
-    document.querySelector('.selecionado').classList.remove('selecionado')
-    document.getElementById('3').classList.add('selecionado')
+    if(  numeros[2].className == 'selecionado'){
+         numeros[2].classList.remove('selecionado')
+    }
+    else {
+         numeros[2].classList.add('selecionado')
+    }
 }
 function botaoQuatro(){
-    console.log('eita')
-    document.querySelector('.selecionado').classList.remove('selecionado')
-    document.getElementById('4').classList.add('selecionado')
+    if(  numeros[3].className == 'selecionado'){
+         numeros[3].classList.remove('selecionado')
+    }
+    else {
+         numeros[3].classList.add('selecionado')
+    }
 }
 function botaoCinco(){
-    console.log('eita')
-    document.querySelector('.selecionado').classList.remove('selecionado')
-    document.getElementById('5').classList.add('selecionado')
+    if(  numeros[4].className == 'selecionado'){
+         numeros[4].classList.remove('selecionado')
+    }
+    else {
+         numeros[4].classList.add('selecionado')
+    }
 }
